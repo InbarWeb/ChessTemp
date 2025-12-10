3acde8ea3e2689754544d3671ebdc9afc77b85d7
ge4rt5 <geffensc@gmail.com>
2025-12-10 19:32:27 +0200

    IPiece change

--- ChessProject/IPiece.h
+++ ChessProject/IPiece.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "Player.h"
-class IPiece : public Player
+class IPiece
 {
+	public:
+		virtual void move() = 0;
+		virtual int getPosition() = 0;
+		virtual void setPosition() = 0;
+		virtual int* getLegalMoves() = 0;
+	protected:
+		int xPos;
+		int yPos;
+		int* legalMoves;
 };
 
