7261022d6972b70bd547bbf30891f7f6f4ea4699
ge4rt5 <geffensc@gmail.com>
2025-12-11 18:58:22 +0200

    change piece

--- ChessProject/IPiece.cpp
+++
@@ -1,1 +0,0 @@
-#include "IPiece.h"
--- ChessProject/IPiece.h
+++ ChessProject/IPiece.h
@@ -3,13 +3,14 @@
 class IPiece
 {
 	public:
-		virtual void move() = 0;
+		virtual void move(int pointSource, int pointDst) = 0;
 		virtual int getPosition() = 0;
-		virtual void setPosition() = 0;
+		virtual void setSrcPosition(int pointSource) = 0;
+		virtual void setDstPosition(int pointDst) = 0;
 		virtual int* getLegalMoves() = 0;
 	protected:
-		int xPos;
-		int yPos;
-		int* legalMoves;
+		int _pointSource;
+		int _pointDst;
+		int* _legalMoves;
 };
 
