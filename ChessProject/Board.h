406b46d4a01649aec7bf5763c5036f867ed142fc
unknown <inbar.kitai@gmail.com>
2025-12-11 18:59:25 +0200

    added board v1

--- ChessProject/Board.h
+++ ChessProject/Board.h
@@ -1,6 +1,22 @@
 #pragma once
+#include "Player.h"
+#include "Piece.h"
+#include <string>
 class Board
 {
-	
+public:
+	Board();
+	~Board();
+	Player* getWhite();
+	Player* getBlack();
+	void initBoard();
+	void clearBoard();
+	int getPieceIndex(std::string pos);
+
+
+protected:
+	Player* white;
+	Player* black;
+	Piece* board[];
 };
 
